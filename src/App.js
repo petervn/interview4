d0969242eeaa364ca82e0b7746ef2975caa3f303
petervn <nguyenquanghai712@gmail.com>
2023-01-09 13:51:54 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Counter = React.memo(({ value, children, onClick }) => {
+  console.log("Render: ", children);
+
+  return (
+    <button onClick={onClick}>
+      {children}: {value}
+    </button>
+  );
+});
+
+const App = () => {
+  const [count1, setCount1] = React.useState(0);
+  const [count2, setCount2] = React.useState(0);
+
+  const increaseCounter1 = () => {
+    setCount1((count1) => count1 + 1);
+  };
+
+  const increaseCounter2 = () => {
+    setCount1((count2) => count1 + 1);
+  };
+
+  return (
+    <>
+      <Counter value={count1} onClick={increaseCounter1}>
+        Counter 1
+      </Counter>
+      <Counter onClick={increaseCounter2}>Coutner 2</Counter>
+    </>
+  );
+};
+
+export default App;
